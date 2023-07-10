181c29a59979712bf1a67d0a54f1bd903bda729b
SanjarKeySkills <forest2004@mail.ru>
2023-07-10 15:21:05 +0600

    add some new

---
+++ src/component/add.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+function App() {
+    const [state, getState] = useState;
+    return (
+        <div>
+            "I will back"
+        </div>
+    )
+}
